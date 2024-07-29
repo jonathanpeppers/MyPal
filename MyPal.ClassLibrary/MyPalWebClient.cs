5d66853d040338b53e5f675f8c0a29ef1466c08e
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-07-29 10:24:29 -0500

    "My Pal" insults my headshot

--- MyPal.ClassLibrary/MyPalWebClient.cs
+++ MyPal.ClassLibrary/MyPalWebClient.cs
@@ -22,4 +22,13 @@ public class MyPalWebClient
         _client = new AzureOpenAIClient(new Uri("https://icropenaiservice2.openai.azure.com/"), new ApiKeyCredential(apiKey));
         _chat = _client.GetChatClient("icrgpt-4o");
     }
+
+    public async Task<string> SendImage(string url)
+    {
+        var result = await _chat.CompleteChatAsync([
+            new SystemChatMessage("You are a funny character that enjoys insulting your friends in a very fun way. All insults are kid-friendly, insult the photo appropriately:"),
+            new UserChatMessage(ChatMessageContentPart.CreateImageMessageContentPart(new Uri(url))),
+        ]);
+        return result.Value.Content[0].ToString();
+    }
 }
--- MyPal.Console/Program.cs
+++ MyPal.Console/Program.cs
@@ -1,5 +1,5 @@
 ﻿using MyPal.ClassLibrary;
 
 var client = new MyPalWebClient();
-
-
+var response = await client.SendImage("https://m.media-amazon.com/images/I/A1UZj5ww2YL._SY450_CR112%2C0%2C450%2C450_.jpg");
+Console.WriteLine(response);
