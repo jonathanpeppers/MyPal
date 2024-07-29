f9c481d4f040804b52b2df634332f0240937f673
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-07-29 15:29:54 -0500

    Pick front-facing camera

--- MyPal.MauiApp/MainPage.xaml.cs
+++ MyPal.MauiApp/MainPage.xaml.cs
@@ -1,9 +1,40 @@
-﻿namespace MyPal.MauiApp;
+﻿using CommunityToolkit.Maui.Core.Primitives;
+
+namespace MyPal.MauiApp;
 
 public partial class MainPage : ContentPage
 {
+    readonly CancellationTokenSource source = new();
+
     public MainPage()
     {
         InitializeComponent();
+    }
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        var cameras = await _camera.GetAvailableCameras(source.Token);
+
+        // Try to find a front camera
+        foreach (var camera in cameras)
+        {
+            if (camera.Position == CameraPosition.Front)
+            {
+                _camera.SelectedCamera = camera;
+                return;
+            }
+        }
+
+        // Then just grab first, if no front
+        _camera.SelectedCamera = cameras.FirstOrDefault();
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+
+        source.Cancel();
     }
 }
