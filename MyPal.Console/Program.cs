acf7c03ad170f68e71bac1063ed37509b5d2f8f0
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-07-29 11:33:44 -0500

    I like "Fable"

--- MyPal.Console/Program.cs
+++ MyPal.Console/Program.cs
@@ -5,18 +5,14 @@ var client = new MyPalWebClient();
 var response = await client.SendImage("https://m.media-amazon.com/images/I/A1UZj5ww2YL._SY450_CR112%2C0%2C450%2C450_.jpg");
 Console.WriteLine(response);
 
-foreach (var voice in client.GetVoices())
+var stream = await client.TextToSpeech(response, "Fable");
+var path = @"D:\Downloads\MyPal.mp3";
+using (var fileStream = File.Create(path))
 {
-    var stream = await client.TextToSpeech(response, voice);
-    var path = @"D:\Downloads\MyPal.mp3";
-    using (var fileStream = File.Create(path))
-    {
-        await stream.CopyToAsync(fileStream);
-    }
-
-    Console.WriteLine($"Playing {voice} voice...");
-    var player = new Player();
-    await player.Play(path);
-
-    Console.ReadLine();
+    await stream.CopyToAsync(fileStream);
 }
+
+var player = new Player();
+await player.Play(path);
+
+Console.ReadLine();
