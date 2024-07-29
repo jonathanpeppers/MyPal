7d704701c6e4e9da2a93352243ee15ec1ed7c2eb
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-07-29 10:13:59 -0500

    MyPalWebClient

---
+++ MyPal.ClassLibrary/MyPalWebClient.cs
@@ -0,0 +1,25 @@
+﻿using Azure.AI.OpenAI;
+using OpenAI.Chat;
+using System.ClientModel;
+
+namespace MyPal.ClassLibrary;
+
+public class MyPalWebClient
+{
+    readonly AzureOpenAIClient _client;
+    readonly ChatClient _chat;
+
+    public MyPalWebClient(string apiKey = "")
+    {
+        if (string.IsNullOrEmpty (apiKey))
+        {
+            apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY")!;
+        }
+        if (string.IsNullOrEmpty(apiKey))
+        {
+            ArgumentNullException.ThrowIfNull(apiKey, nameof(apiKey));
+        }
+        _client = new AzureOpenAIClient(new Uri("https://icropenaiservice2.openai.azure.com/"), new ApiKeyCredential(apiKey));
+        _chat = _client.GetChatClient("icrgpt-4o");
+    }
+}
--- MyPal.Console/Program.cs
+++ MyPal.Console/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyPal.ClassLibrary;
+
+var client = new MyPalWebClient();
+
+
