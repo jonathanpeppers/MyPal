b9b9ab84d05df0eda7dd08e48c26950a056074c4
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-07-29 14:39:22 -0500

    Fix MAUI template for MauiApp namespace

--- MyPal.MauiApp/Platforms/Windows/App.xaml.cs
+++ MyPal.MauiApp/Platforms/Windows/App.xaml.cs
@@ -19,7 +19,7 @@ namespace MyPal.MauiApp.WinUI
             this.InitializeComponent();
         }
 
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+        protected override Microsoft.Maui.Hosting.MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
 
 }
