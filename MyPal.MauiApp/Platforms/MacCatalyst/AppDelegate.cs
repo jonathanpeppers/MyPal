dbcd16f1dc77004524da7bc84db5f3f38a858d6c
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-07-30 08:38:18 -0500

    Fix iOS/Catalyst

--- MyPal.MauiApp/Platforms/MacCatalyst/AppDelegate.cs
+++ MyPal.MauiApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -5,6 +5,6 @@ namespace MyPal.MauiApp
     [Register("AppDelegate")]
     public class AppDelegate : MauiUIApplicationDelegate
     {
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+        protected override Microsoft.Maui.Hosting.MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
 }
