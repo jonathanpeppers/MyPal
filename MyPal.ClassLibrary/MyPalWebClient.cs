506fe6ba4305114072741c7e4762bc53ccc6214a
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-07-31 09:30:09 -0500

    Smaller image, trying to speed up request
    This does get the image size to more like 4MB (down from 5MB), and request time to gpt4-o is more like 9 seconds (down from 10 seconds).

--- MyPal.ClassLibrary/MyPalWebClient.cs
+++ MyPal.ClassLibrary/MyPalWebClient.cs
@@ -36,7 +36,7 @@ public class MyPalWebClient
     {
         var data = await BinaryData.FromStreamAsync(stream);
         var result = await _chat.CompleteChatAsync([
-            new SystemChatMessage("You are a funny character that enjoys insulting your friends in a very fun way. All insults are kid-friendly, insult the photo appropriately using details as much as possible:"),
+            new SystemChatMessage("You are a funny character that enjoys insulting your friends in a very fun way. Do not comment about the photo being blurry, as it is purposely low-resolution. All insults are kid-friendly, insult the photo appropriately using details as much as possible:"),
             new UserChatMessage(ChatMessageContentPart.CreateImageMessageContentPart(data, "image/jpg", ImageChatMessageContentPartDetail.Auto)),
         ]);
         return result.Value.Content[0].ToString();
