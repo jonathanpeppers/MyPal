b200d039b6870e1c0f7b8a63afb707c5b7590050
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-10-25 12:06:14 -0500

    Removed unused OpenAI APIs

--- MyPal.ClassLibrary/MyPalWebClient.cs
+++ MyPal.ClassLibrary/MyPalWebClient.cs
@@ -11,8 +11,6 @@ namespace MyPal.ClassLibrary;
 public partial class MyPalWebClient
 {
     readonly AzureOpenAIClient _client;
-    readonly ChatClient _chat;
-    readonly AudioClient _audio;
     readonly RealtimeConversationClient _realtime;
 
     public MyPalWebClient(string? apiKey = "", bool hd = false)
@@ -30,121 +28,7 @@ public partial class MyPalWebClient
             ArgumentNullException.ThrowIfNull(apiKey, nameof(apiKey));
         }
         _client = new AzureOpenAIClient(new Uri("https://mypalopenai.openai.azure.com/openai/realtime?api-version=2024-10-01-preview&deployment=gpt-4o-realtime-preview"), new ApiKeyCredential(apiKey));
-        _chat = _client.GetChatClient("gpt-4o");
-        _audio = _client.GetAudioClient(hd ? "tts-hd" : "tts");
         _realtime = _client.GetRealtimeConversationClient("gpt-4o-realtime-preview");
-    }
-
-    public Task<string> SendImageAsync(string filePath, bool insult)
-    {
-        using var stream = File.OpenRead(filePath);
-        return SendImageAsync(stream, insult);
-    }
-
-    const string Prompt = "You are a funny character that enjoys {0}ing your friends in a very fun way. Do not comment about the photo being blurry, as it is purposely low-resolution. Don't use ellipsis such as '...' or emojis, and punctuate sentences with only a single '?', '.' or '!'. All {0}s are kid-friendly, {0} the photo appropriately using details as much as possible:";
-
-    public async Task<string> SendImageAsync(Stream stream, bool insult)
-    {
-        var data = await BinaryData.FromStreamAsync(stream);
-        var result = await _chat.CompleteChatAsync([
-            new SystemChatMessage(string.Format(Prompt, insult ? "insult" : "compliment")),
-            new UserChatMessage(ChatMessageContentPart.CreateImagePart(data, "image/jpg", ChatImageDetailLevel.Auto)),
-        ]);
-        return result.Value.Content[0].ToString() ?? "";
-    }
-
-    public string[] GetVoices()
-    {
-        return [
-            nameof(GeneratedSpeechVoice.Alloy),
-            nameof(GeneratedSpeechVoice.Echo),
-            nameof(GeneratedSpeechVoice.Fable),
-            nameof(GeneratedSpeechVoice.Nova),
-            nameof(GeneratedSpeechVoice.Onyx),
-            nameof(GeneratedSpeechVoice.Shimmer),
-        ];
-    }
-
-    public async Task<Stream> TextToSpeechAsync(string text, string voice)
-    {
-        text = text.Trim();
-        var result = await _audio.GenerateSpeechAsync(text, ToVoice(voice), new SpeechGenerationOptions { ResponseFormat = GeneratedSpeechFormat.Mp3 });
-        return result.Value.ToStream();
-    }
-
-    static GeneratedSpeechVoice ToVoice(string voice)
-    {
-        GeneratedSpeechVoice speechVoice;
-        switch (voice)
-        {
-            case "Alloy":
-                speechVoice = GeneratedSpeechVoice.Alloy;
-                break;
-            case "Echo":
-                speechVoice = GeneratedSpeechVoice.Echo;
-                break;
-            case "Fable":
-                speechVoice = GeneratedSpeechVoice.Fable;
-                break;
-            case "Nova":
-                speechVoice = GeneratedSpeechVoice.Nova;
-                break;
-            case "Onyx":
-                speechVoice = GeneratedSpeechVoice.Onyx;
-                break;
-            case "Shimmer":
-                speechVoice = GeneratedSpeechVoice.Shimmer;
-                break;
-            default:
-                throw new InvalidOperationException($"Unable to find voice named: {voice}!");
-        }
-
-        return speechVoice;
-    }
-
-    public async IAsyncEnumerable<Stream> SendImageStreaming(string filePath, string voice, bool insult)
-    {
-        using var stream = File.OpenRead(filePath);
-        await foreach (var item in SendImageStreaming(stream, voice, insult))
-        {
-            yield return item;
-        }
-    }
-
-    [GeneratedRegex(@"[\.|\?|\!]", RegexOptions.CultureInvariant)]
-    private static partial Regex PunctuationRegex();
-
-    public async IAsyncEnumerable<Stream> SendImageStreaming(Stream stream, string voice, bool insult)
-    {
-        var data = await BinaryData.FromStreamAsync(stream);
-        string text = "";
-        await foreach (StreamingChatCompletionUpdate result in _chat.CompleteChatStreamingAsync([
-            new SystemChatMessage(string.Format(Prompt, insult ? "insult" : "compliment")),
-            new UserChatMessage(ChatMessageContentPart.CreateImagePart(data, "image/jpg", ChatImageDetailLevel.Auto)),
-        ]))
-        {
-            foreach (var item in result.ContentUpdate)
-            {
-                if (string.IsNullOrEmpty(item.Text))
-                    continue;
-
-                text += item.Text;
-
-                var match = PunctuationRegex().Match(text);
-                if (match.Success)
-                {
-                    string tts = text[..(match.Index + match.Length)].Trim();
-                    Console.WriteLine("TTS: " + text);
-                    yield return await TextToSpeechAsync(tts, voice);
-                    text = text[(match.Index + match.Length)..];
-                }
-            }
-        }
-
-        // If any leftover text, convert to speech
-        text = text.Trim();
-        if (!string.IsNullOrEmpty(text))
-            yield return await TextToSpeechAsync(text, voice);
     }
 
     /// <summary>
